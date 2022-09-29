17e937d958a2d627e16415e9ba9423d9800752e0
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2022-09-28 23:18:20 -0300

    feat: new backend files

--- backend/src/main/java/com/cloudinfinity/backend/Start.java
+++
@@ -1,7 +0,0 @@
-package com.cloudinfinity.backend;
-
-public class Start {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
---
+++ backend/src/test/java/com/cloudinfinity/pokemonapi/PokemonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudinfinity.pokemonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
