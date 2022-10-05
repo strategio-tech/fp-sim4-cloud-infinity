bdab7edf1e714a8f8b51b5ca642d0b79bd462445
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2022-10-05 14:47:00 -0400

    Update PokemonapiApplicationTests.java
    Changed tests

--- backend/src/test/java/com/cloudinfinity/pokemonapi/PokemonapiApplicationTests.java
+++ backend/src/test/java/com/cloudinfinity/pokemonapi/PokemonapiApplicationTests.java
@@ -20,49 +20,45 @@ import org.openqa.selenium.chrome.*;
 @NotThreadSafe
 @TestInstance(value = Lifecycle.PER_CLASS)
 class PokemonapiApplicationTests {
-	// private WebDriver driver;
+	 private WebDriver driver;
 	
-	// @BeforeAll
-	// public void setUp() {
-	// 	System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-	// 	// WebDriverManager.chromedriver().setup();
-	// 	ChromeOptions options = new ChromeOptions();
-	// 	options.addArguments("--no-sandbox");
-	// 	options.addArguments("--disable-dev-shm-usage");
-	// 	options.addArguments("--headless");
-	// 	driver = new ChromeDriver(options);
-	// 	// driver = new FirefoxDriver();
-	// }
+	 @BeforeAll
+	 public void setUp() {
+	 	System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+	 	 ebDriverManager.chromedriver().setup();
+	 	ChromeOptions options = new ChromeOptions();
+	 	options.addArguments("--no-sandbox");
+	 	options.addArguments("--disable-dev-shm-usage");
+	 	options.addArguments("--headless");
+	 	driver = new ChromeDriver(options);
+	 	// driver = new FirefoxDriver();
+	 }
 	
-	// @Test
-	// void checkSmeargle() {
-	// 	driver.get("http://pokedex-bkt.s3-website-us-east-1.amazonaws.com/");
-	// 	WebElement submit = driver.findElement(By.id("searchBar"));
-	// 	WebElement searchBar = driver.findElement(By.id("searchText"));
-	// 	searchBar.sendKeys("235");
-	// 	submit.submit();
-	// 	WebElement test = driver.findElement(By.className("pokemon-name"));
-	// 	String tester = test.getText();
-	// 	while(tester.equals("Loading...")) {
-			
-	// 	}
-	// 	System.out.println(tester);
-	// 	assert(tester.equals("Smeargle"));
+	 @Test
+	 void checkSmeargle() {
+	 	driver.get("http://pokedex-bkt.s3-website-us-east-1.amazonaws.com/");
+	 	WebElement submit = driver.findElement(By.id("searchBar"));
+	 	WebElement searchBar = driver.findElement(By.id("searchText"));
+	 	searchBar.sendKeys("235");
+	 	submit.submit();
+	 	WebElement test = driver.findElement(By.className("pokemon-name"));
+	 	String tester = test.getText();
+	 	assert(tester.equals("Smeargle"));
 
-	// }
+	 }
 	
-	// @Test
-	// void checkOutofBounds() {
-	// 	driver.get("http://pokedex-bkt.s3-website-us-east-1.amazonaws.com/");
-	// 	WebElement submit = driver.findElement(By.id("searchBar"));
-	// 	WebElement searchBar = driver.findElement(By.id("searchText"));
-	// 	searchBar.sendKeys("930");
-	// 	submit.submit();
-	// 	String tester = driver.switchTo().alert().getText();
-	// 	assert(tester.equals("This pokemon does not exist"));
-	// 	driver.switchTo().alert().dismiss();
+	 @Test
+	 void checkOutofBounds() {
+	 	driver.get("http://pokedex-bkt.s3-website-us-east-1.amazonaws.com/");
+	 	WebElement submit = driver.findElement(By.id("searchBar"));
+	 	WebElement searchBar = driver.findElement(By.id("searchText"));
+	 	searchBar.sendKeys("930");
+	 	submit.submit();
+	 	String tester = driver.switchTo().alert().getText();
+	 	assert(tester.equals("This pokemon does not exist"));
+		driver.switchTo().alert().dismiss();
 		
 
-	// }
+	 }
 
 }
